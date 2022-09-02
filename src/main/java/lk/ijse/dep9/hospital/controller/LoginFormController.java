26714e7bd259ebcbbcb1637f0edc9eebc406da32
Pubudu Janith <pubudujanith123@gmail.com>
2022-09-02 19:06:53 +0530

    Implement data validations

--- src/main/java/lk/ijse/dep9/hospital/controller/LoginFormController.java
+++ src/main/java/lk/ijse/dep9/hospital/controller/LoginFormController.java
@@ -4,13 +4,36 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 
 public class LoginFormController {
     public JFXTextField txtUsername;
     public JFXPasswordField txtPassword;
     public JFXButton btnLogin;
+    public void initialize() {
+        btnLogin.setDefaultButton(true);
+    }
 
     public void btnLogin_OnAction(ActionEvent actionEvent) {
-
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+        if (username.isBlank()) {
+            new Alert(Alert.AlertType.ERROR,"Username cannot be empty").show();
+            txtUsername.requestFocus();
+            txtUsername.selectAll();
+            return;
+        }
+        else if (password.isBlank()) {
+            new Alert(Alert.AlertType.ERROR,"Password cannot be empty").show();
+            txtPassword.requestFocus();
+            txtPassword.selectAll();
+            return;
+        }
+        if (!username.matches("[0-9A-Za-z]+")) {
+            new Alert(Alert.AlertType.ERROR,"Invalid login credentials").show();
+            txtUsername.requestFocus();
+            txtUsername.selectAll();
+            return;
+        }
     }
 }
