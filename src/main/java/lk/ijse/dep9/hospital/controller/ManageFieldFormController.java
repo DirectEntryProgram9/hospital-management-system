0c721356e391efcbd81b9606ca508773db53337f
Pubudu Janith <pubudujanith123@gmail.com>
2022-09-05 17:26:34 +0530

    Close #10

---
+++ src/main/java/lk/ijse/dep9/hospital/controller/ManageFieldFormController.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep9.hospital.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+
+public class ManageFieldFormController {
+    public JFXTextField txtInput;
+    public JFXButton btnSave;
+    public JFXButton btnNew;
+    public JFXButton btnDelete;
+    public ListView lstView;
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnNew_OnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
+
+    }
+}
