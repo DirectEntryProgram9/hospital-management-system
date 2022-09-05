f7af5e1001825636bacaadf01d5e96897804f480
Pubudu Janith <pubudujanith123@gmail.com>
2022-09-05 17:03:55 +0530

    Close #9

---
+++ src/main/java/lk/ijse/dep9/hospital/controller/ProfileManagementFormController.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep9.hospital.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+
+public class ProfileManagementFormController {
+    public JFXButton btnAddProfile;
+    public JFXButton btnModifyProfile;
+    public JFXButton btnDeleteProfile;
+
+    public void btnAddProfile_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnModifyProfile_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteProfile_OnAction(ActionEvent actionEvent) {
+    }
+}
