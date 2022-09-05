d9d87f98d8ad3c04492db0711914f2254d9b0afc
Pubudu Janith <pubudujanith123@gmail.com>
2022-09-05 16:55:31 +0530

    Close #8

---
+++ src/main/java/lk/ijse/dep9/hospital/controller/SettingsFormController.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep9.hospital.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+
+public class SettingsFormController {
+    public JFXButton btnManagePrices;
+    public JFXButton btnAddDiscount;
+    public JFXButton btnAddField;
+    public JFXButton btnChangePassword;
+    public JFXButton btnAbout;
+    public JFXTextArea txtArea;
+
+    public void btnManagePrices_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddDiscount_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddField_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnChangePassword_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAbout_OnAction(ActionEvent actionEvent) {
+    }
+}
